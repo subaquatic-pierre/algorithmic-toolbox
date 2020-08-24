3475440b82b1aa6e90040df5ac03b6e610dcd839
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-08-24 17:04:05 +0400

    completed quick sort improve

--- week_4_divide_algorithms/improve_quicksort/4_3_improve_quicksort.py
+++ week_4_divide_algorithms/improve_quicksort/4_3_improve_quicksort.py
@@ -2,9 +2,23 @@
 import sys
 import random
 
+
 def partition3(a, l, r):
-    #write your code here
-    pass
+    x, j, t = a[l], l, r
+    i = j
+
+    while i <= t:
+        if a[i] < x:
+            a[j], a[i] = a[i], a[j]
+            j += 1
+
+        elif a[i] > x:
+            a[t], a[i] = a[i], a[t]
+            t -= 1
+            i -= 1  # remain in the same i in this case
+        i += 1
+    return j, t
+
 
 def partition2(a, l, r):
     x = a[l]
@@ -22,15 +36,15 @@ def randomized_quick_sort(a, l, r):
         return
     k = random.randint(l, r)
     a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    # use partition3
+    lt, gt = partition3(a, l, r)
+    randomized_quick_sort(a, l, lt - 1)
+    randomized_quick_sort(a, gt + 1, r)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
     randomized_quick_sort(a, 0, n - 1)
     for x in a:
-        print(x, end=' ')
+        print(x, end=" ")
