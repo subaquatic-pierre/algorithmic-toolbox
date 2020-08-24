9b662e3d1bf928c5ec7c332e0fa6bcf9c919c199
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-08-24 15:54:57 +0400

    start improve quicksort

---
+++ week_4_divide_algorithms/improve_quicksort/minimal_test_data.py
@@ -0,0 +1,15 @@
+data1 = {
+    "name": 1,
+    "param_1": 950,
+    "param_2": 400,
+    "list_1": [200, 375, 550, 750],
+}
+data2 = {
+    "name": 2,
+    "param_1": 90,
+    "param_2": 400,
+    "list_1": [200, 300, 550, 750],
+}
+
+test_data = [data1, data2]
+
