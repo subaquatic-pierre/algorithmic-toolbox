290df1c3b0d68c745aca4cd62bd06140425f0ecf
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-09-01 09:01:46 +0400

    untracked files

---
+++ week_5_dynamic_programming/primitive_calculator/implementations.py
@@ -0,0 +1,16 @@
+def fast_algorithm(self, *args, **kwargs):
+    # solve the problem here
+    solution = 42
+
+    result = sulution
+
+    return result
+
+
+def working_algorithm(self, *args, **kwargs):
+    # solve the problem here
+    solution = 42
+
+    result = sulution
+
+    return result
