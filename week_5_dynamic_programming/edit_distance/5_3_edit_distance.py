6915f98b153de9b9655ad5f8b90ea417a3ae1c50
subaquatic-pierre <subaquatic.pierre@gmail.com>
2020-09-02 14:05:20 +0400

    complete edit distance

---
+++ week_5_dynamic_programming/edit_distance/5_3_edit_distance.py
@@ -0,0 +1,35 @@
+# Uses python3
+def edit_distance(s, t):
+    # write your code here
+    # build the matrix
+    D = [0 for elem in range((len(t) + 1))]
+    for i in range(len(D)):
+        D[i] = [0 for l in range(len(s) + 1)]
+
+    # add letters to first col
+    for i in range(1, len(s) + 1):
+        D[0][i] = i
+
+    # add letters to first col of each row
+    for i in range(1, len(t) + 1):
+        D[i][0] = i
+
+    # compare letters
+    for row in range(1, len(t) + 1):
+        t_letter = t[row - 1]
+        for col in range(1, len(s) + 1):
+            s_letter = s[col - 1]
+            hor_dist = D[row][col - 1] + 1
+            vert_dist = D[row - 1][col] + 1
+            if s_letter == t_letter:
+                diag_dist = D[row - 1][col - 1]
+            else:
+                diag_dist = D[row - 1][col - 1] + 1
+
+            D[row][col] = min(vert_dist, hor_dist, diag_dist)
+
+    return D[-1][-1]
+
+
+if __name__ == "__main__":
+    print(edit_distance(input(), input()))
---
+++ week_5_dynamic_programming/edit_distance/minimal_test_data.py
@@ -0,0 +1,15 @@
+data1 = {
+    "name": 1,
+    "s": "editing",
+    "t": "distance",
+}
+
+data2 = {
+    "name": 2,
+    "param_1": 90,
+    "param_2": 400,
+    "list_1": [200, 300, 550, 750],
+}
+
+test_data = [data1, data2]
+
